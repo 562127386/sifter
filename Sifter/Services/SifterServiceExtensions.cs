42f86649731d4bdef938db665ae9ef2b67fccb14
French Grandpa <pepijn.pinckers@hotmail.nl>
2019-09-22 11:08:27 +0200

    Added extension method for Sift()

---
+++ Sifter/Services/SifterServiceExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using JetBrains.Annotations;
+using Sifter.Models;
+
+
+namespace Sifter.Services {
+
+    [PublicAPI]
+    public static class SifterServiceExtensions {
+
+        [NotNull]
+        public static IQueryable<T> Sift<T>(
+            [NotNull] this IQueryable<T> query,
+            [NotNull] ISifterService sifter,
+            [NotNull] SifterModel sifterModel
+        ) {
+            return sifter.Sift(query, sifterModel);
+        }
+
+    }
+
+}
